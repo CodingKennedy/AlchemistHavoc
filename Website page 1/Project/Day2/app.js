32e168ee68bbbe9c61a31fd7bfd3e38d0b3967a6
spiritninja66 <spiritninja66@aol.com>
2014-08-13 21:19:13 -0400

    initial commit of project.

---
+++ Website page 1/Project/Day2/app.js
@@ -0,0 +1,10 @@
+var $el = $(".example:first"), text = $el.text(),
+    words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+    html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+  $(this).delay(i*500).fadeIn(700);
+});
