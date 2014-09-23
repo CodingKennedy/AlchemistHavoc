daa7789f6e85ec3aa9f4125768d3ad5c85908d6d
SpiritJinja66 <spiritninja66@aol.com>
2014-09-23 11:33:10 -0400

    Creating portfolio page.

---
+++ Website page 1/Portfolio/Newyork/app.js
@@ -0,0 +1,32 @@
+ $(document).ready(function() {
+        $('.menu-link').bigSlide();
+    });
+
+ $( ".more-bio" ).click(function() {
+  $( ".more-bio-text" ).toggle( "slow", function() {
+
+  });
+});
+
+$( ".more-text" ).click(function() {
+  $( ".more-bio-summary" ).toggle( "slow", function() {
+
+  });
+});
+
+$( ".Williams" ).click(function() {
+  $( ".Williamsburg-summary" ).toggle( "slow", function() {
+
+  });
+});
+
+$( ".Circle" ).click(function() {
+  $( ".Columbus-Circle-summary" ).toggle( "slow", function() {
+
+  });
+});
+$( ".park" ).click(function() {
+  $( ".Central-Park-summary" ).toggle( "slow", function() {
+
+  });
+});
